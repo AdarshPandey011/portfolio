147500ce7bc086f7332214e0391fe5b81a5e2dcd
Adarsh Pandey <AdarshPandey011>
2024-11-24 16:02:08 +0530

    deleted

--- app/head.tsx
+++ app/head.tsx
@@ -2,21 +2,21 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
+      <meta name="description" content="I'm Adarsh Pandey, a Full-Stack Developer, passionate about crafting intuitive and scalable web applications. My development journey started in college and has since evolved into a drive for blending creative problem-solving with technical expertise to build seamless digital experiences." />
       <meta name="keywords" content="jigar sable, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
       <meta property="og:site_name" content="Jigar Sable's Portfolio" />
-      <meta property="og:title" content="Portfolio | Jigar Sable - Full Stack Developer" />
+      <meta property="og:title" content="Portfolio | Adarsh Pandey - Full Stack Developer" />
       <meta property="og:description"
-        content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta property="og:url" content="https://jigarsable.vercel.app" />
-      <meta property="og:image" content="https://jigarsable.vercel.app/portfolio-fork.png" />
-      <link rel="canonical" href="https://jigarsable.vercel.app" />
+        content="I'm Adarsh Pandey, a Full-Stack Developer, passionate about crafting intuitive and scalable web applications. My development journey started in college and has since evolved into a drive for blending creative problem-solving with technical expertise to build seamless digital experiences." />
+      <meta property="og:url" content="" />
+      <meta property="og:image" content="" />
+      <link rel="canonical" href="" />
       <meta name="author" content="Jigar Sable" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://jigarsable.vercel.app" />
-      <meta name="application-name" content="Portfolio | Jigar Sable - Full Stack Developer" />
-      <meta name="apple-mobile-web-app-title" content="Portfolio | Jigar Sable - Full Stack Developer" />
+      <meta name="twitter:site" content="" />
+      <meta name="application-name" content="Portfolio | Adarsh Pandey - Full Stack Developer" />
+      <meta name="apple-mobile-web-app-title" content="Portfolio | Adarsh Pandey - Full Stack Developer" />
       <link rel="icon" href="/favicon.ico" />
       <title>Portfolio | Jigar Sable - Full Stack Developer</title>
     </>
