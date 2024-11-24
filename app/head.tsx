04d95d723c7d9ae6ccebca1b96657d2b8f18d499
Adarsh Pandey <AdarshPandey011>
2024-11-24 16:26:53 +0530

    Added Title

--- app/head.tsx
+++ app/head.tsx
@@ -3,15 +3,15 @@ export default function Head() {
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="I'm Adarsh Pandey, a Full-Stack Developer, passionate about crafting intuitive and scalable web applications. My development journey started in college and has since evolved into a drive for blending creative problem-solving with technical expertise to build seamless digital experiences." />
-      <meta name="keywords" content="jigar sable, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
-      <meta property="og:site_name" content="Jigar Sable's Portfolio" />
+      <meta name="keywords" content="Adarsh Pandey, portfolio, Adarsh, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta property="og:site_name" content="Adarsh Pandey Portfolio" />
       <meta property="og:title" content="Portfolio | Adarsh Pandey - Full Stack Developer" />
       <meta property="og:description"
         content="I'm Adarsh Pandey, a Full-Stack Developer, passionate about crafting intuitive and scalable web applications. My development journey started in college and has since evolved into a drive for blending creative problem-solving with technical expertise to build seamless digital experiences." />
       <meta property="og:url" content="" />
       <meta property="og:image" content="" />
       <link rel="canonical" href="" />
-      <meta name="author" content="Jigar Sable" />
+      <meta name="author" content="Adarsh Pandey" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="" />
